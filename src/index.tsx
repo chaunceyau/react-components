bcd4a42e10057f0d499be2cf0410fb184ef783d3
Austin Chauncey <chauncey.au@gmail.com>
2021-01-09 23:12:26 -0800

    feature: basic statistic card

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,5 @@ export { FormUpload } from './components/form/elements/upload'
 export { FormOptions } from './components/form/elements/options'
 //
 export { LoadingSpinner } from './components/misc/spinner'
+//
+export { Statistic } from './components/statistic'
