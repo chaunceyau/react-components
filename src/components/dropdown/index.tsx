e3b2beb077e2d5f3d544fca6a9d0d158b9be5309
Austin Chauncey <chauncey.au@gmail.com>
2021-01-31 16:38:16 -0800

    :bug: close select options on click outside

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,5 +1,6 @@
 import { Transition } from '@headlessui/react'
 import React from 'react'
+import { useClickOutside } from '../../hooks/useClickOutside'
 
 interface Link {
   to: string
@@ -14,12 +15,17 @@ interface DropdownMenuProps {
 export function DropdownMenu(props: DropdownMenuProps) {
   const [open, setOpen] = React.useState(false)
 
+  const popupRef = React.useRef<HTMLDivElement>(null)
+
+  useClickOutside(popupRef, () => setOpen(false))
+
   return (
     <div className='relative inline-block text-left'>
       <div>
         <button
           type='button'
           className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500'
+          // TODO: probably update this...
           id='options-menu'
           aria-haspopup='true'
           aria-expanded={open}
@@ -51,7 +57,10 @@ export function DropdownMenu(props: DropdownMenuProps) {
         leaveFrom='transform opacity-100 scale-100'
         leaveTo='transform opacity-0 scale-95'
       >
-        <div className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5'>
+        <div
+          ref={popupRef}
+          className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5'
+        >
           <div
             className='py-1'
             role='menu'
